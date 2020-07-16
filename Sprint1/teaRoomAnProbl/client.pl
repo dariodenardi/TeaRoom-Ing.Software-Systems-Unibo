22b9e15fdf2370591454522057d47161d4e4539c
Dario De Nardi <dariodenardi@hotmail.it>
2020-07-16 22:03:14 +0200

    Fix3 Sprint2

--- Sprint1/teaRoomAnProbl/client.pl
+++
@@ -1,8 +0,0 @@
-%====================================================================================
-% client description   
-%====================================================================================
-context(ctxclient, "localhost",  "TCP", "8060").
-context(ctxtearoom, "127.0.0.1",  "TCP", "8040").
- qactor( waitermind, ctxtearoom, "external").
-  qactor( smartbell, ctxtearoom, "external").
-  qactor( client, ctxclient, "it.unibo.client.Client").
--- Sprint1/teaRoomAnReq/client.pl
+++
@@ -1,8 +0,0 @@
-%====================================================================================
-% client description   
-%====================================================================================
-context(ctxclient, "localhost",  "TCP", "8060").
-context(ctxtearoom, "127.0.0.1",  "TCP", "8040").
- qactor( waiter, ctxtearoom, "external").
-  qactor( smartbell, ctxtearoom, "external").
-  qactor( client, ctxclient, "it.unibo.client.Client").
