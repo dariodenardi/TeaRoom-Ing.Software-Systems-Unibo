515b8a7f03c907fef426db0f0339e38f743d853d
Dario De Nardi <dariodenardi@hotmail.it>
2020-07-22 16:06:49 +0200

    Fix13 Sprint2

--- prove.basicrobot/trysubscribe.pl
+++
@@ -1,5 +0,0 @@
-%====================================================================================
-% trysubscribe description   
-%====================================================================================
-context(ctxsubscriber, "127.0.0.1",  "TCP", "8050").
- qactor( subscriberprova, ctxsubscriber, "it.unibo.subscriberprova.Subscriberprova").
